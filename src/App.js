5aab743626831ce45d05e5e0063faec7911b00ec
Michael Ha <mic.ha.el@me.com>
2022-08-31 10:42:02 -0700

    working routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import logo from './logo.svg';
 import "./App.css";
 import Home from './Home';
-import Contact from './Contact';
+import About from './About';
 import Scheduler from './Scheduler';
+import {Routes, Route, BrowserRouter, Link} from 'react-router-dom';
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
-      <Scheduler />
+      <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/About" element={<About/>} />
+        <Route exact path="/Scheduler" element={<Scheduler/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
