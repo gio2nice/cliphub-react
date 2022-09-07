b14a0e85322d616311163b3e7601a2d9c782e364
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-06 19:32:05 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Home from './Home';
 import About from './About';
 import Scheduler from './Scheduler';
-import {Routes, Route, BrowserRouter, Link} from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
 import NavBar from "./NavBar";
 
 function App() {
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import About from './About';
 
 function Home(){
     return(
         <div>
-            <h1>Welcome to Cliphub</h1>
+            <h1 className='home'>Welcome to Cliphub</h1>
+            <About />
         </div>
     )
 }
