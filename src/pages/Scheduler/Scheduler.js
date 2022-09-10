12f8626959878933090fe70fc6461a4646b5a567
Noah Carralero <nole2kgames@gmail.com>
2022-09-10 02:03:20 -0400

    fixed scheduler

--- src/pages/Scheduler/Scheduler.js
+++ src/pages/Scheduler/Scheduler.js
@@ -26,10 +26,6 @@ class Scheduler extends React.Component {
           prefill={this.props.prefill}
           onModalClose={() => this.setState({ isOpen: false })}
           open={this.state.isOpen}
-          /*
-           * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-           * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-           */
           rootElement={document.getElementById("root")}
         />
       </div>
