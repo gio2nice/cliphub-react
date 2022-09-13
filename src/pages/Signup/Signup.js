42de63e6050148a91c918040162cc24c0bbb1880
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-13 16:03:57 -0700

    d

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 // import API from "../../utils/API";
 
+
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
